7d9ba9545bb96e662aa864a42b191aa4842fbd8e
Catio <71468014+CatioYT@users.noreply.github.com>
2022-07-21 21:46:25 +0300

    smol stuffs

--- main.lua
+++ main.lua
@@ -7,16 +7,18 @@ function MainLoop()
 Input = io.read()
 
 if Input == "sleep" then
-RandomTemp = math.random(1,3)
-if RandomTemp == 1 or 2 then
+RandomTemp = math.random(1,2)
+if RandomTemp == 1 then
 if Energy < 100 then
 
 Energy = Energy + 100 - Energy
 print("You Slept Well! Back to full energy!")
+elseif Energy == 100 then
+print("You Arent Tired!")
 end
 end
 
-if RandomTemp == 3 then
+if RandomTemp == 2 then
 Energy = Energy - 10
 print("You didn't sleep well! You lost energy!")
 print("You now have " .. Energy .. " energy!")
