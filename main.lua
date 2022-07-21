8466f3dd049ab08efe5f8038c102fae475c7c9b6
Catio <71468014+CatioYT@users.noreply.github.com>
2022-07-21 21:40:59 +0300

    Bad Sleep System Added

--- main.lua
+++ main.lua
@@ -1,16 +1,24 @@
-Energy = 100
+Energy = 30
+RandomTemp = 0
 
 
-
-
+print("Hello World")
 function MainLoop()
 Input = io.read()
 
 if Input == "sleep" then
+RandomTemp = math.random(1,3)
+if RandomTemp == 1 or 2 then
 if Energy < 100 then
-print(Energy - 100)
-Energy = Energy - 100
+
+Energy = Energy + 100 - Energy
 print("You Slept Well! Back to full energy!")
+end
+end
+
+if RandomTemp == 3 then
+Energy = Energy - 10
+print("You didn't sleep well! You lost energy!")
 end
 end
 
