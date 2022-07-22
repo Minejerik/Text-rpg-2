858e27aec805e4fd514c1c18f71900941cbb6265
Catio <71468014+CatioYT@users.noreply.github.com>
2022-07-22 09:24:02 +0300

    promos and comments

--- main.lua
+++ main.lua
@@ -9,6 +9,7 @@ RandomTemp = 0
 
 print("Hello World")
 function MainLoop()
+io.write("What Would you like to do?\n")
 Input = io.read()
 
 if Input == "sleep" then
@@ -17,6 +18,10 @@ end
 
 if Input == "work" then
 work.work()
+end
+
+if Input == "askforpromotion" then
+work.askpromo()
 end
 
 MainLoop()
--- work.lua
+++ work.lua
@@ -14,6 +14,7 @@ Worktitles[10] = "Dwight Shrute"
 Worktitles[11] = "Assistant Manager"
 Worktitles[12] = "Manager"
 Worktitles[13] = "CEO"
+--this adds work systems
 work.work = function()
 if Energy >= 5 then
 Money = Money + 5*Promolevel
@@ -21,8 +22,25 @@ Energy = Energy - 5
 print("You Went to work as a(n) ".. Worktitles[Promolevel].." !")
 print("You Earned "..5*Promolevel.." Dollars!")
 else
-
-
+print("You Dont Have Enough Energy!")
+print("Get Some Sleep!")
+end
+end
+--this adds the promotion system
+work.askpromo = function()
+if Energy >= 15 then
+Energy = Energy - 15
+RandomTemp = math.random(0,Promolevel)
+if RandomTemp == 0 then
+Promolevel = Promolevel+1
+print("You Got Promoted to "..Worktitles[Promolevel].."!")
+print("You Now Earn "..5*Promolevel.." Dollars!")
+end
+if not RandomTemp == 0 then
+print("You Didnt Get Promoted!")
+end
+elseif Energy <= 15 then
+print("You Are to Tired to Ask for a promotion")
 end
 end
 return work
